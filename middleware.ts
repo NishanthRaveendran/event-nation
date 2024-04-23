ff4e608aca64ca05aea3118b93e6fd3309f9214d
Nishanth <nishanthrv13@gmail.com>
2024-04-23 16:12:36 -0400

    minor fixes

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,7 @@ export default authMiddleware({
         '/events/:id',
         '/api/webhooks/clerk',
         '/api/webhooks/stripe',
+        '/api/webhooks(.*)',
         '/api/uploadthing'
     ],
     ignoredRoutes: [
