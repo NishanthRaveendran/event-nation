db5c263a7a3b38d8331361c469db3aa63c31f843
Nishanth <nishanthrv13@gmail.com>
2024-05-02 14:03:54 -0400

    dropdown changes

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -5,13 +5,12 @@ const MONGODB_URI = process.env.MONGODB_URI;
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-  console.log("DATABSEDDDDDDDDD");
   if (cached.conn) return cached.conn;
 
   if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    dbName: 'evently',
+    dbName: 'eventNation',
     bufferCommands: false,
   })
 
